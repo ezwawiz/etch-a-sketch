d958f9569a93980ab0710864192d51ac39a65eb9
Zach Ezwawi <ezwawiz@gmail.com>
2022-08-25 23:21:55 -0400

    Add resizing for grid cells and implement regular and randomized rainbow colouring

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,75 +1,56 @@
+const REGULAR = 'black';
+let RAINBOW = '#' + Math.floor(Math.random()*16777215).toString(16);
+let SHADER = 'grey';
+
 const container = document.querySelector('#container');
 
-for(i=0;i<=16;i++) {
-    const row = document.createElement('div');
-    // row.style.width = '20px';
-    // row.style.height = '20px';
-    // row.style.flex = '0 0 auto';
-    // row.style.border = '2px solid black';
-    row.style.display = 'inline-block';
-    container.appendChild(row);
-    // createBox();
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (button.id == 'btn-regular') {
+            createGrid(16, REGULAR) 
+         } else if (button.id == 'btn-rainbow') {
+             createGrid(16, RAINBOW); 
+         } else if (button.id == 'btn-shader') {
+            createGrid(16, SHADER) 
+         }  
+    })
+});
 
-
-    for(j=0; j<=16;j++) {
-        const column = document.createElement('div');
-        column.classList.add('box');
-        column.style.width = '20px';
-        column.style.height = '20px';
-        column.style.flex = '0 0 auto';
-        column.style.border = '1px solid black';
-        column.style.margin = '-1px -1px 0 0';
-        column.addEventListener('mouseover', () => {
-            column.style.backgroundColor = 'black';
-        })
-        row.appendChild(column);
-
-        // createBox();
-
-        // if (j == 16) {
-        //     // const next = document.createElement('br');
-        //     // container.appendChild(next);
-        //     box.style.float = 'right'; 
-            
-        // }
+function reset() {
+    while(container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
     }
 }
 
+function createGrid(size, colour) {
+    reset();
 
-function createBox() {
-    const box = document.createElement('div');
-    // box.style.margin = 0;
-    // box.style.padding = 0;
-    box.style.width = '20px';
-    box.style.height = '20px';
-    box.style.flex = '0 0 auto';
-    box.style.border = '2px solid black';
-    container.appendChild(box);
+    for(i=0;i<size;i++) {
+        const row = document.createElement('div');
+        row.style.display = 'inline-block';
+        container.appendChild(row);
+
+    for(j=0; j<size;j++) {
+        const column = document.createElement('div');
+        column.classList.add('box');
+        column.style.width = `${500 / size}px`;
+        column.style.height = `${500 / size}px`;
+        column.style.flex = '1 0 auto';
+        column.style.border = '1px solid black';
+        column.style.margin = '-1px -1px 0 0';
+        column.addEventListener('mouseover', () => {
+            if (colour == RAINBOW) {
+                column.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+            } else if (colour == SHADER) {
+                column.style.backgroundColor = colour;
+            } else {
+                column.style.backgroundColor = colour;
+            }
+
+        });
+        row.appendChild(column);
+    }
+}
 };
 
-// container.addEventListener('mouseover', () => {
-//     var item = document.getElementsByClassName('box');
-//     item.style.backgroundColor = 'pink';
-// });
-
-// const box = document.createElement('div');
-// box.style.width = '100px';
-// box.style.height = '100px';
-// box.style.backgroundColor = 'pink';
-// container.appendChild(box);
-
-// function test() {
-//   const container = document.querySelector("#container");
-
-//     let box = document.createElement("div");
-//     box.classList.add = "box";
-//     box.style.width = "100px";
-//     body.style.height = "100px";
-//     box.style.border = "4px solid black";
-//     box.style.backgroundColor = 'pink';
-
-//     container.appendChild(box);
-
-// }
-
-// test();
